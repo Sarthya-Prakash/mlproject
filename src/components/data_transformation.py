5a6a03923a6d1a2b7057d8d88ab103a9b0af8116
Sarthya-Prakash <sarthyaprakash2007@gmail.com>
2026-09-16 19:09:02 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -87,14 +87,14 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
-                input_feature_train_df,np.array(target_feature_train_df)
+                input_feature_train_arr, np.array(target_feature_train_df).reshape(-1, 1)
             ]
             test_arr = np.c_[
-                input_feature_test_df,np.array(target_feature_test_df)
+                input_feature_test_arr, np.array(target_feature_test_df).reshape(-1, 1)
             ]
 
             logging.info(f"Saved preprocessing object.")
